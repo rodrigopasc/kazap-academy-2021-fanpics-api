d70a05289ee66ae202379f1a44239e017c8c1edb
Rodrigo Paschoaletto <rodrigopaschoaletto@gmail.com>
2021-07-14 21:31:35 -0300

    feat(posts): add crud

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,8 +1,86 @@
+const httpStatus = require('http-status')
+const { Post } = require('../models')
+const { safeObjectId } = require('../helpers')
+
 const methods = {
     async list(request, response) {
-        response.status(200).json({
-            title: 'Kazap Academy 2021 - funcionou!'
-        })
+        const post = new Post()
+
+        try {
+            const posts = await post.list({ deletedAt: { $exists: false } })
+
+            response.status(httpStatus.OK).json(posts)
+        } catch(error) {
+            response.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+        }
+    },
+
+    async create(request, response) {
+        const { title, url, authorId, description } = request.body
+
+        const post = new Post()
+
+        if (!title || !url) {
+            return response.status(httpStatus.BAD_REQUEST).json({ error: 'The fields "title" and "url" are both required.' })
+        }
+
+        try {
+            const insertedObject = await post.insertOne({ title, url, authorId: safeObjectId(authorId), description, createdAt: Date.now(), updatedAt: Date.now() })
+
+            response.status(httpStatus.CREATED).json(insertedObject)
+        } catch (error) {
+            response.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+        }
+    },
+
+    async show(request, response) {
+        const { id } = request.params
+        const convertedObjectId = safeObjectId(id)
+
+        const post = new Post()
+
+        try {
+            const postToReturn = await post.findOne({  _id: convertedObjectId  })
+            
+            response.status(httpStatus.OK).json(postToReturn)
+        } catch (error) {
+            response.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+        }
+    },
+
+    async update(request, response) {
+        const { id } = request.params
+        const convertedObjectId = safeObjectId(id)
+        const { title, url, authorId, description } = request.body
+
+        if (!title || !url) {
+            return response.status(httpStatus.BAD_REQUEST).json({ error: 'The fields "title" and "url" are both required.' })
+        }
+
+        const post = new Post()
+
+        try {
+            const updatedObject = await post.updateOne({ _id: convertedObjectId }, { title, url, authorId: safeObjectId(authorId), description, updatedAt: Date.now() })
+            
+            response.status(httpStatus.OK).json(updatedObject)
+        } catch (error) {
+            response.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+        }
+    },
+
+    async destroy(request, response) {
+        const { id } = request.params
+        const convertedObjectId = safeObjectId(id)
+
+        const post = new Post()
+
+        try {
+            const destroyedObject = await post.updateOne({ _id: convertedObjectId }, { deletedAt: Date.now() })
+            
+            response.status(httpStatus.NO_CONTENT).json()
+        } catch (error) {
+            response.status(httpStatus.INTERNAL_SERVER_ERROR).json(error)
+        }
     }
 }
 
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,5 +2,9 @@ const router = require('express').Router()
 const { posts } = require('../controllers')
 
 router.get('/', posts.list)
+router.post('/', posts.create)
+router.get('/:id', posts.show)
+router.patch('/:id', posts.update)
+router.delete('/:id', posts.destroy)
 
 module.exports = router
